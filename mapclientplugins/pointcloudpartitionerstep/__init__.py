a9475ac826c328eeace19ba25f7e664bc057321a
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-10-03 13:30:45 +1300

    Set version to 0.5.0.

--- mapclientplugins/pointcloudpartitionerstep/__init__.py
+++ mapclientplugins/pointcloudpartitionerstep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.4.2'
+__version__ = '0.5.0'
 __author__ = 'Timothy Salemink'
 __stepname__ = 'Point Cloud Partitioner'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.pointcloudpartitionerstep'
